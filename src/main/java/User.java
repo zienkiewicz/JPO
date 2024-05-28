00d716315c523c14f2b7e5defb1502ac44ccbae0
kacper zienkiewicz <k09028449@gmail.com>
2024-05-28 22:19:09 +0200

    Plik reprezentujący użytkownika

---
+++ src/main/java/User.java
@@ -0,0 +1,18 @@
+
+public class User {
+    private String username;
+    private String hashedPassword;
+
+    public User(String username, String hashedPassword) {
+        this.username = username;
+        this.hashedPassword = hashedPassword;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+}
