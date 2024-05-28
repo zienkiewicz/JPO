b6fbe2008352e772cbd1273dfd1eefbb96a8d989
kacper zienkiewicz <k09028449@gmail.com>
2024-05-28 22:37:50 +0200

    Test czy pakiet dziala i jest gituwa

--- src/main/java/com/example/jpo/HelloApplication.java
+++ src/main/java/com/example/jpo/HelloApplication.java
@@ -7,11 +7,17 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import model.User;
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        User user = new User("Username", "hashedPassword");
+        System.out.println("Username: " + user.getUsername());
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
