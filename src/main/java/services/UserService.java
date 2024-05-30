87c1aa7837f8b0b8fb02e453342861a1e3061c6b
kacper zienkiewicz <k09028449@gmail.com>
2024-05-30 10:29:43 +0200

    UserService cały

--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -8,13 +8,13 @@ import model.User;
 public class UserService {
     private static final String USERS_FILE = "users.txt";
 
-    static public boolean registerUser(String username, String password) {
+    public static boolean registerUser(String username, String password) {
         String hashedPassword = HashUtil.hashPassword(password);
         User user = new User(username, hashedPassword);
         return saveUser(user);
     }
 
-    static private boolean saveUser(User user) {
+    private static boolean saveUser(User user) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(USERS_FILE, true))) {
             writer.write(user.getUsername() + ":" + user.getHashedPassword());
             writer.newLine();
@@ -23,5 +23,32 @@ public class UserService {
             e.printStackTrace();
             return false;
         }
+    }
+
+    private static List<User> loadUsers() {
+        List<User> users = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader((new FileReader(USERS_FILE)))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(":");
+                if (data.length != 2) {
+                    users.add(new User(data[0], data[1]));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
+
+    public static boolean loginUser(String username, String password) {
+        String hashedPassword = HashUtil.hashPassword(password);
+        List<User> users = loadUsers();
+        for (User user : users) {
+            if (user.getUsername().equals(username) && user.getHashedPassword().equals(hashedPassword)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
